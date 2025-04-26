81e7a90eaf292b320dbf511bdf6e285b334745b5
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2025-04-26 13:10:22 -0400

    define pullback

--- Symplectic/Definitions.lean
+++ Symplectic/Definitions.lean
@@ -1,6 +1,8 @@
 import Mathlib
 import Symplectic.Eval
 
+noncomputable section
+
 open Manifold VectorField
 
 notation3 "∞" => ((⊤ : ℕ∞) : WithTop ℕ∞)
@@ -36,13 +38,34 @@ structure IsSymplecticForm : Prop where
   alternating : ∀ p : M, ∀ X : (T[E]M) p, ω p X X = 0
   nondegenerate : ∀ p : M, Function.Bijective (ω p : E → (E →L[ℝ] ℝ))
   closed : ∀ (X Y Z : Cₛ^∞⟮𝓘(ℝ, E); E, (T[E]M)⟯), ∀ p : M,
-    eval (X p) (fun p ↦ ω p (Y p) (Z p))
-    - eval (Y p) (fun p ↦ ω p (X p) (Z p))
-    + eval (Z p) (fun p ↦ ω p (X p) (Y p))
-    + ω p (mlieBracket 𝓘(ℝ, E) X Y p) (Z p)
-    - ω p (mlieBracket 𝓘(ℝ, E) X Z p) (Y p)
-    + ω p (mlieBracket 𝓘(ℝ, E) Y Z p) (X p) = 0
+    eval (X p) (fun q ↦ ω q (Y q) (Z q))
+    - eval (Y p) (fun q ↦ ω q (X q) (Z q))
+    + eval (Z p) (fun q ↦ ω q (X q) (Y q))
+    + ω p (mlieBracket _ X Y p) (Z p)
+    - ω p (mlieBracket _ X Z p) (Y p)
+    + ω p (mlieBracket _ Y Z p) (X p) = 0
 
 theorem IsSymplecticForm.eval_swap (h : IsSymplecticForm ω) :
-    ∀ p : M, ∀ X Y : (T[E]M) p, ω p X Y = - ω p Y X :=
+    ∀ p : M, ∀ X Y : (T[E]M) p, ω p X Y = - ω p Y X := by
   sorry
+
+variable
+  -- F a real Banach space
+  {F : Type} [NormedAddCommGroup F] [NormedSpace ℝ F]
+  -- N a smooth manifold modelled on F
+  {N : Type} [TopologicalSpace N] [ChartedSpace F N] [IsManifold 𝓘(ℝ, F) ∞ N]
+
+variable (f : ContMDiffMap 𝓘(ℝ, F) 𝓘(ℝ, E) N M ∞)
+
+/-- The pullback of a smooth 1-form `η` along a smooth map `f : N → M`. -/
+def ContMDiffSection.pullback1 : Cₛ^∞⟮𝓘(ℝ, F); F →L[ℝ] ℝ, T*[F]N⟯ where
+  toFun (x : N) := η (f x) ∘L mfderiv 𝓘(ℝ, F) 𝓘(ℝ, E) f x
+  contMDiff_toFun := sorry
+
+/-- The pullback of a smooth 2-tensor `ω` along a smooth map `f : N → M`. -/
+def ContMDiffSection.pullback2 : Cₛ^∞⟮𝓘(ℝ, F); F →L[ℝ] F →L[ℝ] ℝ, T^2[F]N⟯ where
+  toFun (x : N) :=
+    have : (T*[E]M) (f x) →L[ℝ] (T*[F]N) x :=
+      (ContinuousLinearMap.compL ℝ F E ℝ).flip (mfderiv 𝓘(ℝ, F) 𝓘(ℝ, E) f x)
+    this ∘L (ω (f x) ∘L mfderiv 𝓘(ℝ, F) 𝓘(ℝ, E) f x)
+  contMDiff_toFun := sorry
